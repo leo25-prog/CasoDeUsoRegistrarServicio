84e78e3075985788e66596da46e00f34aa273567
leo25-prog <leo.lr.xhido@gmail.com>
2022-05-18 00:50:05 -0500

    Primer avance del caso de uso, crud de ejemplo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button, Container, Modal, ModalBody, MaodalHeader, FormGroup, ModalFooter} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, personaje: "Naruto", anime: "Naruto"},
+  { id: 2, personaje: "Goku", anime: "Dragon Ball"},
+  { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin"},
+  { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece"},
+  { id: 5, personaje: "Edward Elric", anime: "Fullmetal Alchemist: BrotherHood"},
+  { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-oh!"},
+];
+
+class App extends React.Component {
+state = {
+  data: data
+}
+  render(){
+    return(
+      <>
+      <Container>
+        <br />
+        <Button color = "success" > Insertar Nuevo Personaje </Button>
+        <br /> <br />
+        
+        <Table>
+          <thead><tr><th>Id</th>
+          <th>Personaje</th>
+          <th>Anime</th>
+          <th>Acciones</th></tr></thead>
+          <tbody>
+            {this.state.data.map((elemento)=>(
+              <tr>
+                <td>{elemento.id}</td>
+                <td>{elemento.personaje}</td>
+                <td>{elemento.anime}</td>
+                <td><Button color = "primary"> Editar</Button>{"  "}
+                    <Button color = "danger"> Eliminar</Button>
+                </td>
+              </tr>
+            ))}
+
+          </tbody>
+        </Table>
+      </Container>
+      </>
+    );
+  }
 }
 
 export default App;
